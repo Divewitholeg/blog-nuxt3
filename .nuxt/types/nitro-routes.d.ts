fe0b9f9f8fa808969392addd71b896b14fc65afa
vuewebdev <rybniko@gmail.com>
2023-04-21 18:09:58 +0300

    Deployment article updated

--- .nuxt/types/nitro-routes.d.ts
+++ .nuxt/types/nitro-routes.d.ts
@@ -21,7 +21,7 @@ declare module 'nitropack' {
     '/api/_content/query': {
       'get': Simplify<Serialize<Awaited<ReturnType<typeof import('../../node_modules/@nuxt/content/dist/runtime/server/api/query').default>>>>
     }
-    '/api/_content/cache.1682084360290.json': {
+    '/api/_content/cache.1682089689538.json': {
       'get': Simplify<Serialize<Awaited<ReturnType<typeof import('../../node_modules/@nuxt/content/dist/runtime/server/api/cache').default>>>>
     }
     '/api/_content/navigation/:qid/**:params': {
